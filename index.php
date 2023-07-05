117943b1baf1477b3c4ed88994d75ec795d899fd
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-07-05 09:58:14 +0200

    [DOCS] esercizio completato

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+    // ?php echo $hotel['name']?
 
     $hotels = [
 
@@ -41,38 +41,54 @@
 
     ];
 
-    var_dump($hotels)
 ?>
-
-
-?>
-
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-    <div>
-    <ul>
-        <!-- cicliamo l'array associativo con il ciclo foreach -->
-        <?php foreach($hotels as $hotel ) {?>
-            <!-- inseriamo i list item che verra ciclato  -->
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <!-- table inserita -->
+                <table class="table">
+                    <thead>
+                        <th>Nome</th>
+                        <th>Descrizione</th>
+                        <th>Parcheggio</th>
+                        <th>Voto</th>
+                        <th>distanza dal centro</th>
+                    </thead>
+                    <tbody>
+                        <!-- vengono ciclati i tr -->
+                        <?php foreach($hotels as $hotel ) {?>
+                            <tr>
+                                <!-- inseriti le chiavi con i propri valori all'interno dei td -->
+                                <td><?php echo $hotel['name'] ?></td>
+                                <td><?php echo $hotel['description'] ?></td>
+                                <td><?php echo $hotel['parking'] ?></td>
+                                <td><?php echo $hotel['vote'] ?></td>
+                                <td><?php echo $hotel['distance_to_center'] ?></td>                        
+                            </tr>
+                        
+                        <?php } ?>
 
-            <li>
-                <?php echo $hotel ?>
-                
-
-            </li>
-
+                    </tbody>
+                    
+                    
+                </table>
 
 
-        <?php } ?>
-    </ul>
+            </div>
+        </div>
     </div>
+
+      
     
     
 </body>
